0d5d45afe0b191eac6bbdd003738fd851f91fd9a
ArkanozHawk <mayolo_123@hotmail.com>
2019-03-01 01:26:32 -0600

    cambios realizados

--- Control-Escolar/Control-Escolar/bitacora.cs
+++ Control-Escolar/Control-Escolar/bitacora.cs
@@ -148,7 +148,8 @@ namespace Control_Escolar
                 Document pdfDoc = new Document(PageSize.LETTER, 10f, 10f, 10f, 0f);
                 PdfWriter.GetInstance(pdfDoc, stream);
                 pdfDoc.Open();
-                iTextSharp.text.Image imagen = iTextSharp.text.Image.GetInstance("C:/Users/Tevi/Documents/Gestionde proyectos/controlEscolar-master/logo1.jpg");
+                //iTextSharp.text.Image imagen = iTextSharp.text.Image.GetInstance("C:/Users/Tevi/Documents/Gestionde proyectos/controlEscolar-master/logo1.jpg");
+                iTextSharp.text.Image imagen = iTextSharp.text.Image.GetInstance("C:/PRUEBAS_GIT/controlEscolar/Control-Escolar/logo.png");
                 imagen.BorderWidth = 0;
                 imagen.SetAbsolutePosition(0, 500);
                 float percentage = 0.0f;
@@ -205,121 +206,6 @@ namespace Control_Escolar
                 stream.Close();
 
             }
-        }
-
-            // Creamos el documento con el tamaño de página tradicional
-            Document doc = new Document(PageSize.LETTER);
-            // Indicamos donde vamos a guardar el documento
-            PdfWriter writer = PdfWriter.GetInstance(doc,
-                                        new FileStream(@"C:\Users\Lizeth\Desktop\prueba.pdf", FileMode.Create));
-
-            // Le colocamos el título y el autor
-            // **Nota: Esto no será visible en el documento
-            doc.AddTitle("Bitacora de Inicio de Sesión");
-            doc.AddCreator("Instituto Rodolfo Neri Vela");
-
-            // Abrimos el archivo
-            doc.Open();
-
-            //Image png = Image.GetInstance(imagepath + "/logo-sep.png");
-            // png.ScalePercent(24f);
-            //doc.Add(png);
-
-            // Escribimos el encabezamiento en el documento
-            doc.Add(new Paragraph("Instituto Rodolfo Neri Vela"));
-            doc.Add(new Paragraph("Vicente Guerrero 49, Barrios Históricos, 39300. Acapulco, Gro."));
-            doc.Add(new Paragraph("Clave:12DPT0003N       Nivel: Primaria"));
-            doc.Add(new Paragraph("Bitacora de Inicio de Sesión"));
-            doc.Add(Chunk.NEWLINE);
-
-            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
-
-            // Creamos una tabla que contendrá el nombre, apellido y país 
-            // de nuestros visitante.
-            PdfPTable tblPrueba = new PdfPTable(4);
-            tblPrueba.WidthPercentage = 100;
-            tblPrueba.HorizontalAlignment = 1; // 0 = Izquierda, 1 = Centro, 2 = Derecha
-
-            // Configuramos el título de las columnas de la tabla
-            PdfPCell clusuario = new PdfPCell(new Phrase("Usuario", _standardFont));
-            clusuario.BorderWidth = 0;
-            clusuario.BorderWidthBottom = 0.75f;
-
-            PdfPCell clfecha = new PdfPCell(new Phrase("Fecha", _standardFont));
-            clfecha.BorderWidth = 0;
-            clfecha.BorderWidthBottom = 0.75f;
-
-            PdfPCell clhoraent = new PdfPCell(new Phrase("Hora de Entrada", _standardFont));
-            clhoraent.BorderWidth = 0;
-            clhoraent.BorderWidthBottom = 0.75f;
-
-            PdfPCell clhorasal = new PdfPCell(new Phrase("Hora de Salida", _standardFont));
-            clhorasal.BorderWidth = 0;
-            clhorasal.BorderWidthBottom = 0.75f;
-
-            // Añadimos las celdas a la tabla
-            tblPrueba.AddCell(clusuario);
-            tblPrueba.AddCell(clfecha);
-            tblPrueba.AddCell(clhoraent);
-            tblPrueba.AddCell(clhorasal);
-
-            // Llenamos la tabla con información
-            clusuario = new PdfPCell(new Phrase("Mayolo", _standardFont));
-            clusuario.BorderWidth = 0;
-
-            clfecha = new PdfPCell(new Phrase("Suaste", _standardFont));
-            clfecha.BorderWidth = 0;
-
-            clhoraent = new PdfPCell(new Phrase("Puerto Rico", _standardFont));
-            clhoraent.BorderWidth = 0;
-
-            clhorasal = new PdfPCell(new Phrase("Puerto Rico", _standardFont));
-            clhorasal.BorderWidth = 0;
-
-            // Añadimos las celdas a la tabla
-            tblPrueba.AddCell(clusuario);
-            tblPrueba.AddCell(clfecha);
-            tblPrueba.AddCell(clhoraent);
-            tblPrueba.AddCell(clhorasal);
-
-            clusuario = new PdfPCell(new Phrase("Martha", _standardFont));
-            clusuario.BorderWidth = 0;
-
-            clfecha = new PdfPCell(new Phrase("Teran", _standardFont));
-            clfecha.BorderWidth = 0;
-
-            clhoraent = new PdfPCell(new Phrase("México", _standardFont));
-            clhoraent.BorderWidth = 0;
-
-            clhorasal = new PdfPCell(new Phrase("México", _standardFont));
-            clhorasal.BorderWidth = 0;
-
-            tblPrueba.AddCell(clusuario);
-            tblPrueba.AddCell(clfecha);
-            tblPrueba.AddCell(clhoraent);
-            tblPrueba.AddCell(clhorasal);
-
-            // Finalmente, añadimos la tabla al documento PDF y cerramos el documento
-            doc.Add(tblPrueba);
-
-            doc.Close();
-            writer.Close();
-
-            MessageBox.Show("¡PDF creado!");
-        }
-
-        private void BtnCerrar_Click(object sender, EventArgs e)
-        {
-            string HoraSalida = Convert.ToString(DateTime.Now);
-            int idAccess = sesion.idAcceso;
-            //string conexion = "server=localhost;uid=root;pwd=digi3.0;database=nerivela";
-            string conexion = "server=localhost;uid=root;database=nerivela";
-            string inserta_bitacora = "UPDATE bitacora SET HoraSalida = '" + HoraSalida + "' where idAcceso = " + idAccess + ";";
-            obj.insBitacora(conexion, inserta_bitacora);
-            System.Threading.Thread login = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadProc));
-
-            login.Start();
-            this.Close();
         }
     }
 }
