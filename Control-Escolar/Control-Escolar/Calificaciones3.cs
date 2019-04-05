d86aa1c1547a81f74c1d838fce6d7aa21d9a985e
joshtevi <coche770816@hotmail.com>
2019-04-05 03:17:16 -0600

    fdff

--- Control-Escolar/Control-Escolar/Calificaciones3.cs
+++ Control-Escolar/Control-Escolar/Calificaciones3.cs
@@ -44,8 +44,9 @@ namespace Control_Escolar
         public static void ThreadGenerarBoletas()
 
         {
-
+            Application.Run(new GenerarBoletas());
         }
+
         private void btnCerrar_Click_1(object sender, EventArgs e)
         {
             string HoraSalida = Convert.ToString(DateTime.Now);
