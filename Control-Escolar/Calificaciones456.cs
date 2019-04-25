813aedd45ed0160b9c0c1c55f6124b80846770f6
joshtevi <coche770816@hotmail.com>
2019-04-25 12:07:25 -0500

    modificar solo director

--- Control-Escolar/Calificaciones456.cs
+++ Control-Escolar/Calificaciones456.cs
@@ -4147,6 +4147,30 @@ namespace Control_Escolar
                 cmbdiagedfisica.Text = diag_EdFis;
                 cmbdiaginasis.Text = diag_Inasis;
 
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string query3 = "SELECT * FROM  `personal`  where  Usuario =" + "'" + sesion.Usuario + "' ";
+            MessageBox.Show(query3);
+            conn = new MySqlConnection(conexion);
+            conn.Open();
+
+            com = new MySqlCommand(query3, conn);
+
+            MySqlDataReader myreader3 = com.ExecuteReader();
+            myreader3.Read();
+
+
+
+
+            sesion.cargo = Convert.ToString(myreader3["cargo"]);
+            MessageBox.Show(sesion.cargo);
+            if (sesion.cargo == "Secretario(a)")
+            {
 
                 // BLOQUEO SEPTIEMBRE
                 if (EspDiag.Text != "")
@@ -4654,14 +4678,10 @@ namespace Control_Escolar
                     cmbdiaginasis.Enabled = false;
                 }
 
-                //MessageBox.Show(sept_Esp.ToString());
-            }
-
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
             }
         }
+                //MessageBox.Show(sept_Esp.ToString());
+      
 
         //------------------------------------Metodo para buscar la materia-----------------------------------------------
         public void buscarmateria()
